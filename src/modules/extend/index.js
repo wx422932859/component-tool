682b15cdcda593fef6bff5aa9908967ceccef04b
wang.xin <422932859@qq.com>
2023-09-25 21:32:01 +0800

    添加水印扩展功能

---
+++ src/modules/extend/index.js
@@ -0,0 +1,14 @@
+import WaterMark from './water-mark/water-mark';
+
+/**
+ * 扩展功能
+ */
+const Extend = {};
+
+/**
+ * 水印
+ * @see WaterMark
+ */
+Extend.WaterMark = WaterMark;
+
+export default Extend;
