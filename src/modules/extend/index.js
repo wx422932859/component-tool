0377cd6e81bbb0f8004d1a6a7d5691cab068705c
wang.xin <422932859@qq.com>
2024-04-25 19:37:30 +0800

    优化代码结构

--- src/modules/extend/index.js
+++ src/modules/extend/index.js
@@ -1,18 +1,17 @@
 import WaterMark from './water-mark/water-mark';
 import ScrollBar from './scroll-bar/scroll-bar';
 import FixTheadTable from './fixed-thead-table/fixed-thead-table';
+import Pagination from './pagination/pagination.js';
+import FileUpload from './file-upload/file-upload.js';
+import ScrollTop from './scroll-top/scroll-top.js';
+import FilePreview from './file-preview/file-preview.js';
+import Popup from './popup/popup.js';
 
 /**
  * 扩展功能
  * @exports Extend
  */
 const Extend = {};
-
-/**
- * 水印
- * @see WaterMark
- */
-Extend.WaterMark = WaterMark;
 
 /**
  * 滚动条
@@ -25,5 +24,51 @@ Extend.ScrollBar = ScrollBar;
  * @see FixTheadTable
  */
 Extend.FixTheadTable = FixTheadTable;
+
+/**
+ * @member {class} Pagination 页码
+ * @memberof Extend
+ * @static
+ * @see Pagination
+ */
+Extend.Pagination = Pagination;
+
+/**
+ * @member {class} FileUpload 文件上传
+ * @memberof Extend
+ * @static
+ * @see FileUpload
+ */
+Extend.FileUpload = FileUpload;
+
+/**
+ * @member {class} ScrollTop 文件上传
+ * @memberof Extend
+ * @static
+ * @see ScrollTop
+ */
+Extend.ScrollTop = ScrollTop;
+
+/**
+ * @member {class} FilePreview 文件预览
+ * @memberof Extend
+ * @static
+ * @see FilePreview
+ */
+Extend.FilePreview = FilePreview;
+
+/**
+ * @member {class} Popup 弹窗
+ * @memberof Extend
+ * @static
+ * @see Popup
+ */
+Extend.Popup = Popup;
+
+/**
+ * 水印
+ * @see WaterMark
+ */
+Extend.WaterMark = WaterMark;
 
 export default Extend;
--- src/modules/form/index.js
+++ src/modules/form/index.js
@@ -1,16 +1,10 @@
-import '../../assets/iconfont/iconfont.js';
 import Form from './form.js';
 import DropList from './drop-list/drop-list.js';
 import Radio from './radio/radio.js';
 import Checkbox from './checkbox/checkbox.js';
 import Switch from './switch/switch.js';
 import Slider from './slider/slider.js';
-import Pagination from './pagination/pagination.js';
-import FileUpload from './file-upload/file-upload.js';
-import ScrollTop from './scroll-top/scroll-top.js';
-import FilePreview from './file-preview/file-preview.js';
 import MultipleList from './multiple-list/multiple-list.js';
-import Popup from './popup/popup.js';
 
 /**
  * @member {class} DropList 下拉列表
@@ -53,51 +47,11 @@ Form.Switch = Switch;
 Form.Slider = Slider;
 
 /**
- * @member {class} Pagination 页码
- * @memberof Form
- * @static
- * @see Pagination
- */
-Form.Pagination = Pagination;
-
-/**
- * @member {class} FileUpload 文件上传
- * @memberof Form
- * @static
- * @see FileUpload
- */
-Form.FileUpload = FileUpload;
-
-/**
- * @member {class} ScrollTop 文件上传
- * @memberof Form
- * @static
- * @see ScrollTop
- */
-Form.ScrollTop = ScrollTop;
-
-/**
- * @member {class} FilePreview 文件预览
- * @memberof Form
- * @static
- * @see FilePreview
- */
-Form.FilePreview = FilePreview;
-
-/**
  * @member {class} MultipleList 多选下拉列表
  * @memberof Form
  * @static
  * @see MultipleList
  */
 Form.MultipleList = MultipleList;
-
-/**
- * @member {class} Popup 弹窗
- * @memberof Form
- * @static
- * @see Popup
- */
-Form.Popup = Popup;
 
 export default Form;
