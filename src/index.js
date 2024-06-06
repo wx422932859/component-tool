b3a5db13f11d6ef1e4000de074295bc3f6de1bbd
wang.xin <422932859@qq.com>
2024-06-07 00:12:22 +0800

    优化文件引入

--- src/index.js
+++ src/index.js
@@ -16,84 +16,98 @@ import XMLNode from './modules/base/xml-node.js';
 
 /**
  * 对外暴露的全局变量
+ *
  * @namespace
  */
 const LY = {};
 
 /**
  * 接口请求相关方法
+ *
  * @see module:Ajax
  */
 LY.Ajax = Ajax;
 
 /**
  * 常用工具方法
+ *
  * @see module:Util
  */
 LY.Util = Util;
 
 /**
  * 组件引入相关方法
+ *
  * @see module:VC
  */
 LY.VC = VC;
 
 /**
  * 扩展功能
+ *
  * @see module:Extend
  */
 LY.Extend = Extend;
 
 /**
  * 组件基类
+ *
  * @see Component
  */
 LY.Component = Component;
 
 /**
  * 事件总线
+ *
  * @see EventBus
  */
 LY.EventBus = EventBus;
 
 /**
  * 封装 DOM 操作
+ *
  * @see MyNode
  */
 LY.MyNode = MyNode;
 
 /**
  * 监听器
+ *
  * @see Observe
  */
 LY.Observe = Observe;
 
 /**
  * 任务队列
+ *
  * @see TaskQueue
  */
 LY.TaskQueue = TaskQueue;
 
 /**
  * 时间
+ *
  * @see Time
  */
 LY.Time = Time;
 
 /**
  * 观察者
+ *
  * @see Watcher
  */
 LY.Watcher = Watcher;
 
 /**
  * 表单
+ *
  * @see Form
  */
 LY.Form = Form;
 
 /**
  * XML 节点
+ *
  * @see XMLNode
  */
 LY.XMLNode = XMLNode;
