c05c7d60ec5bb97645d002a01a50e8c41324dcb8
wang.xin <422932859@qq.com>
2024-06-23 23:52:23 +0800

    优化弹窗功能

---
+++ test/js/common.js
@@ -0,0 +1,3 @@
+const { Util, Component, Form, Observe, VC, Extend, Time, Ajax } = LY;
+const { DropList, Radio, Switch, Slider, Checkbox, MultipleList } = Form;
+const { WaterMark, ScrollBar, FileUpload, FilePreview, ScrollTop, Popup, Pagination } = Extend;
